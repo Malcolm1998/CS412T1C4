94cd4b5fca0e7b59b3903d9475bea490923eb9f7
Ravenshard <jechmila@ualberta.ca>
2019-11-27 16:56:58 -0700

    Shape detection finished!

--- shapeTesting/constantgreen.py
+++ shapeTesting/constantgreen.py
@@ -24,11 +24,36 @@ def main():
     global symbol_green_mask_orig, symbol_green_mask_good, h, w, d
     rospy.init_node('green')
     # image_sub = rospy.Subscriber('cv_camera/image_raw',
-    #                               Image, image_callback)
-    image_sub = rospy.Subscriber('camera/rgb/image_raw',
-                                      Image, image_callback)
+                                  # Image, logitech_callback)
+    # image_sub = rospy.Subscriber('camera/rgb/image_raw',
+                                      # Image, logitech_callback)
+    image_sub = rospy.Subscriber('usb_cam/image_raw',
+                                    Image, logitech_callback)
     while not shutdown_requested:
         rospy.spin()
+    return
+
+def logitech_callback(msg):
+    global bridge, symbol_green_mask_good, symbol_green_mask_orig, h, w, d
+    image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    secondary_h, secondary_w, secondary_d = image.shape
+    # upper_red_a = numpy.array([20, 255, 255])
+    # lower_red_a = numpy.array([0, 100, 100])
+    # red_mask_a = cv2.inRange(hsv, lower_red_a, upper_red_a)
+
+    # upper_red_b = numpy.array([255, 255, 255])
+    # lower_red_b = numpy.array([150, 100, 100])
+    # red_mask_b = cv2.inRange(hsv, lower_red_b, upper_red_b)
+    # red_mask = cv2.bitwise_or(red_mask_a, red_mask_b)
+    upper_red = numpy.array([177,255, 255])
+    lower_red = numpy.array([0, 150, 50])
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    blur = cv2.medianBlur(red_mask, 7)
+    cv2.imshow("red window",blur)
+    cv2.waitKey(3)
     return
 
 def image_callback(msg):
